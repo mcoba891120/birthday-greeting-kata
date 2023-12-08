5c4c176365a879797351cb543fe646654d4da85a
mcoba891120 <119391593+mcoba891120@users.noreply.github.com>
2023-12-08 16:42:06 +0800

    feat: add create_connection

--- models/db.js
+++ models/db.js
@@ -1,18 +1,29 @@
 require("dotenv").config();
-const mysql = require("mysql12/promise");
+const mysql = require("mysql2/promise");
 const env = process.env.NODE_ENV || "development";
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = process.env;
 const dbConfig = {
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
+  port: DB_PORT,
 };
 
-let dbEnv = dbConfig[env];
+let dbEnv = dbConfig;
 dbEnv.waitForConnections = true;
 dbEnv.connectionLimit = 10;
 
 const pool = mysql.createPool(dbEnv);
 
-module.exports = pool;
+const create_connection = async () => {
+  try {
+    const conn = await pool.getConnection();
+    console.log("Database connection successful!");
+    return conn;
+  } catch (err) {
+    console.error("Database connection error:", err);
+  }
+};
+
+module.exports = { pool, create_connection };
