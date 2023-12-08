d1b7a466fa0e90956a49127ba042e7f32db5897d
mcoba891120 <119391593+mcoba891120@users.noreply.github.com>
2023-12-08 17:09:46 +0800

    refactor: update route path to /v1

--- routes/birthday_greeting_kata_router.js
+++ routes/birthday_greeting_kata_router.js
@@ -2,6 +2,6 @@ var express = require("express");
 var router = express.Router();
 const birthdayGreeting = require("../controllers/birthday_greeting_kata_controller");
 
-router.get("/", birthdayGreeting);
+router.get("/v1", birthdayGreeting);
 
 module.exports = router;
