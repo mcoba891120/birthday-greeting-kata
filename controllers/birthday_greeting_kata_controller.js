8497279206fa6e4dfa91cf6c2b0153b98071fefc
mcoba891120 <119391593+mcoba891120@users.noreply.github.com>
2023-12-08 17:57:17 +0800

    feat: add error handling for empty result in birthdayGreeting

--- controllers/birthday_greeting_kata_controller.js
+++ controllers/birthday_greeting_kata_controller.js
@@ -8,9 +8,11 @@ const birthdayGreeting = async (req, res, next) => {
     month = req.query.month || currentMonth;
     day = req.query.day || currentDay;
     const result = await getBirthdayGreetings(month, day);
+    if (result.length === 0) {
+      return res.status(404).json({ message: "NOT FOUND" });
+    }
     const message = await messageGenerator(result);
     res.status(200).json(message);
-    console.log(message);
   } catch (err) {
     next(err);
   }
