d3c69230e512521ac95cdc7c12a05a0d6f30792f
mcoba891120 <119391593+mcoba891120@users.noreply.github.com>
2023-12-08 17:00:19 +0800

    refactor: refactor routes and add birthday greeting router

--- app.js
+++ app.js
@@ -1,41 +1,48 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var birthdayGreetingRouter = require("./routes/birthday_greeting_kata_router");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/birthday_greeting", birthdayGreetingRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Internal Server Error";
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(errorStatus);
+  res.render("error", {
+    title: "Error",
+    message: errorMessage,
+    error: {
+      status: errorStatus,
+      stack: err.stack,
+    },
+  });
 });
 
 module.exports = app;
