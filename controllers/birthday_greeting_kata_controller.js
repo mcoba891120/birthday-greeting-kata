7f8a1942b57f7631b44a2b49ecda9655e42ebccc
mcoba891120 <119391593+mcoba891120@users.noreply.github.com>
2023-12-08 17:40:13 +0800

    feat: add query parameters for month and day in birthdayGreeting function

--- controllers/birthday_greeting_kata_controller.js
+++ controllers/birthday_greeting_kata_controller.js
@@ -3,7 +3,11 @@ const messageGenerator = require("../utils/message_generator");
 
 const birthdayGreeting = async (req, res, next) => {
   try {
-    const result = await getBirthdayGreetings();
+    const currentMonth = "08";
+    const currentDay = "08";
+    month = req.query.month || currentMonth;
+    day = req.query.day || currentDay;
+    const result = await getBirthdayGreetings(month, day);
     const message = await messageGenerator(result);
     res.status(200).json(message);
     console.log(message);
--- models/birthday_greeting_kata_model.js
+++ models/birthday_greeting_kata_model.js
@@ -1,14 +1,14 @@
 const { pool, create_connection, queryDatabase } = require("./db");
-const moment = require("moment");
 
-const getBirthdayGreetings = async () => {
-  let today = moment().format("YYYY-MM-DD");
-  today = "2023-08-08";
+const getBirthdayGreetings = async (month, day) => {
+  month = month.padStart(2, "0");
+  day = day.padStart(2, "0");
+  const today = `${month}-${day}`;
   try {
     const query = `
-            SELECT * FROM member_records
-            WHERE DATE_FORMAT(date_of_birth, '%m-%d') = DATE_FORMAT(?, '%m-%d')
-        `;
+          SELECT * FROM member_records
+          WHERE DATE_FORMAT(date_of_birth, '%m-%d') = ?
+      `;
     const rows = await queryDatabase(query, [today]);
     return rows;
   } catch (err) {
