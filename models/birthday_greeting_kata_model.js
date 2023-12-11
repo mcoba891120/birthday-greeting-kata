8c82d133d8ad40b0e5f5f31326ba1f74d395b6f6
mcoba891120 <119391593+mcoba891120@users.noreply.github.com>
2023-12-11 22:35:55 +0800

    feat: add MongoDB functionality to birthday greeting model

--- models/birthday_greeting_kata_model.js
+++ models/birthday_greeting_kata_model.js
@@ -1,4 +1,5 @@
 const { queryDatabase } = require("./db");
+const Member = require("./mongodb");
 
 const getBirthdayGreetings = async (month, day) => {
   month = month.padStart(2, "0");
@@ -17,4 +18,28 @@ const getBirthdayGreetings = async (month, day) => {
   }
 };
 
-module.exports = getBirthdayGreetings;
+const addBirthdayMemberMongodb = async (member) => {
+  try {
+    const newMember = new Member(member);
+    await newMember.save();
+  } catch (err) {
+    console.error("Error during database query:", err);
+    throw err;
+  }
+};
+
+const findBirthdayMemberMongodb = async () => {
+  try {
+    const member = await Member.find();
+    console.log("coooll", member);
+    return member;
+  } catch (err) {
+    console.error("Error during database query:", err);
+    throw err;
+  }
+};
+module.exports = {
+  getBirthdayGreetings,
+  addBirthdayMemberMongodb,
+  findBirthdayMemberMongodb,
+};
