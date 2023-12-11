f1aa71021e3960761f619c3899b93b69ae1421e2
mcoba891120 <119391593+mcoba891120@users.noreply.github.com>
2023-12-11 15:54:12 +0800

    refactor: refactor birthdayGreeting functions

--- controllers/birthday_greeting_kata_controller.js
+++ controllers/birthday_greeting_kata_controller.js
@@ -6,10 +6,10 @@ const {
 const validateDate = require("../utils/date_validator");
 const { currentDay, currentMonth } = require("../constants");
 
-const birthdayGreeting = async (req, res, next) => {
+const birthdayGreetingCommon = async (req, res, next, messageGenerator) => {
   try {
-    month = req.query.month || currentMonth;
-    day = req.query.day || currentDay;
+    const month = req.query.month || currentMonth;
+    const day = req.query.day || currentDay;
     if (!validateDate(month, day)) {
       return res.status(400).json({ message: "Input Date is invalid" });
     }
@@ -24,22 +24,12 @@ const birthdayGreeting = async (req, res, next) => {
   }
 };
 
-const birthdayGreeting_gender = async (req, res, next) => {
-  try {
-    month = req.query.month || currentMonth;
-    day = req.query.day || currentDay;
-    if (!validateDate(month, day)) {
-      return res.status(400).json({ message: "Input Date is invalid" });
-    }
-    const result = await getBirthdayGreetings(month, day);
-    if (result.length === 0) {
-      return res.status(404).json({ message: "NOT FOUND" });
-    }
-    const message = await messageGeneratorV2(result);
-    res.status(200).json(message);
-  } catch (err) {
-    next(err);
-  }
+const birthdayGreeting = (req, res, next) => {
+  return birthdayGreetingCommon(req, res, next, messageGenerator);
+};
+
+const birthdayGreeting_gender = (req, res, next) => {
+  return birthdayGreetingCommon(req, res, next, messageGeneratorV2);
 };
 
 module.exports = { birthdayGreeting, birthdayGreeting_gender };
