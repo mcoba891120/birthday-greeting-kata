071f5c108a5d97ca0e2de39b091fa47f670cdd08
mcoba891120 <119391593+mcoba891120@users.noreply.github.com>
2023-12-12 02:45:22 +0800

    feat: add messageGeneratorV2 test

--- tests/message_generator.test.ts
+++ tests/message_generator.test.ts
@@ -1,4 +1,4 @@
-const messageGenerator = require('../utils/message_generator');
+const {messageGenerator,messageGeneratorV2} = require('../utils/message_generator');
 const {Subject} = require('../constants');
 
 test('messageGenerator should return correct messages', async () => {
@@ -11,5 +11,28 @@ test('messageGenerator should return correct messages', async () => {
   ];
 
   const actual = await messageGenerator(result);
+  expect(actual).toEqual(expected);
+});
+
+test('messageGeneratorV2 should return correct messages and promotions', async () => {
+  const result = [
+    { first_name: 'John', gender: 'Male' },
+    { first_name: 'Jane', gender: 'Female' },
+  ];
+
+  const expected = [
+    { 
+      subject: Subject, 
+      message: 'Happy birthday, dear John!', 
+      promotion_message: 'We offer special discount 20% off for the following items: White Wine, iPhone X' 
+    },
+    { 
+      subject: Subject, 
+      message: 'Happy birthday, dear Jane!', 
+      promotion_message: 'We offer special discount 50% off for the following items: Cosmetic, LV Handbags' 
+    },
+  ];
+
+  const actual = await messageGeneratorV2(result);
   expect(actual).toEqual(expected);
 });
