25a6172d8936e025c3be9e691739888f266d18d0
mcoba891120 <119391593+mcoba891120@users.noreply.github.com>
2023-12-12 03:15:30 +0800

    feat: add validateAge function to date_validator.test.js

--- tests/date_validator.test.ts
+++ tests/date_validator.test.ts
@@ -1,5 +1,5 @@
 // /tests/date_validator.test.js
-const validateDate = require('../utils/date_validator');
+const {validateDate,validateAge} = require('../utils/date_validator');
 
 test('validateDate should return true for valid dates', () => {
   expect(validateDate('01', '01')).toBe(true);
@@ -9,4 +9,16 @@ test('validateDate should return true for valid dates', () => {
 test('validateDate should return false for invalid dates', () => {
   expect(validateDate('02', '30')).toBe(false);
   expect(validateDate('04', '31')).toBe(false);
+});
+
+describe('validateAge', () => {
+  it('should return true for elderly people', () => {
+    const date_of_birth = '1956-06-01'; // This person would be 67 in 2023
+    expect(validateAge(date_of_birth)).toBe(true);
+  });
+
+  it('should return false for non-elderly people', () => {
+    const date_of_birth = '1990-05-01'; // This person would be 33 in 2023
+    expect(validateAge(date_of_birth)).toBe(false);
+  });
 });
