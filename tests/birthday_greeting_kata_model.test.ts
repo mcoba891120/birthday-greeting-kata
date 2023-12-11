801d309c2802d7ca38ae397ece9e077653321193
mcoba891120 <119391593+mcoba891120@users.noreply.github.com>
2023-12-12 04:07:14 +0800

    feat: add addBirthdayMemberMongodb and getBirthdayGreetings_mongodb test

--- tests/birthday_greeting_kata_model.test.ts
+++ tests/birthday_greeting_kata_model.test.ts
@@ -1,5 +1,18 @@
-// /tests/birthday_greeting_kata_model.test.js
-const getBirthdayGreetings = require('../models/birthday_greeting_kata_model');
+import mongoose from "mongoose";
+const mockSave = jest.fn();
+const mockFind = jest.fn();
+jest.mock('../models/mongodb', () => {
+  function Member(data) {
+    this.data = data;
+    this.save = mockSave;
+  }
+
+  Member.find = mockFind;
+
+  return Member;
+});
+
+const { getBirthdayGreetings, addBirthdayMemberMongodb, getBirthdayGreetings_mongodb } = require('../models/birthday_greeting_kata_model');
 const db = require('../models/db');
 
 jest.mock('../models/db', () => ({
@@ -14,4 +27,34 @@ test('getBirthdayGreetings should return correct data', async () => {
 
   const actual = await getBirthdayGreetings('01', '01');
   expect(actual).toEqual(mockData);
+});
+
+
+test('addBirthdayMemberMongodb should save member to database', async () => {
+  const member = {
+    name: 'Test',
+    date_of_birth: '1990-01-01',
+    email: 'test@example.com',
+  };
+
+  await addBirthdayMemberMongodb(member);
+
+  expect(mockSave).toHaveBeenCalled();
+});
+
+test('getBirthdayGreetings_mongodb should return members with birthday on specific date', async () => {
+  const members = [
+    { name: 'Test1', date_of_birth: '1990-01-01', email: 'test1@example.com' },
+    { name: 'Test2', date_of_birth: '1990-01-02', email: 'test2@example.com' },
+  ];
+
+  mockFind.mockImplementation(() => Promise.resolve(members));
+
+  const birthdayMembers = await getBirthdayGreetings_mongodb('01', '01');
+
+  expect(birthdayMembers).toEqual(members);
+});
+
+afterAll(() => {
+  mongoose.connection.close();
 });
