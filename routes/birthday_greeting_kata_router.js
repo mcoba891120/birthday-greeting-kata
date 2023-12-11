06489dea223f291065a71992b505aefc8d9c9d8d
mcoba891120 <119391593+mcoba891120@users.noreply.github.com>
2023-12-11 23:09:55 +0800

    feat: add new route for MongoDB member in birthday greeting kata

--- routes/birthday_greeting_kata_router.js
+++ routes/birthday_greeting_kata_router.js
@@ -7,12 +7,14 @@ const {
   birthdayGreeting_fullname,
   birthdayGreetingMemeberAdd_mongodb,
   birthdayGreetingMemeberFind_mongodb,
+  birthdayGreetingMemeber_mongodb,
 } = require("../controllers/birthday_greeting_kata_controller");
 
 router.get("/v1", birthdayGreeting);
 router.get("/v2", birthdayGreeting_gender);
 router.get("/v3", birthdayGreeting_age);
 router.get("/v4", birthdayGreeting_fullname);
+router.get("/v5", birthdayGreetingMemeber_mongodb);
 router.get("/find", birthdayGreetingMemeberFind_mongodb);
 router.post("/add", birthdayGreetingMemeberAdd_mongodb);
 
