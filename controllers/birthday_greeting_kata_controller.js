5e12694d74829c569c979e533077f89eeb47c95e
mcoba891120 <119391593+mcoba891120@users.noreply.github.com>
2023-12-11 15:35:14 +0800

    feat: add birthdayGreeting_gender controller

--- controllers/birthday_greeting_kata_controller.js
+++ controllers/birthday_greeting_kata_controller.js
@@ -1,5 +1,8 @@
 const getBirthdayGreetings = require("../models/birthday_greeting_kata_model");
-const messageGenerator = require("../utils/message_generator");
+const {
+  messageGenerator,
+  messageGeneratorV2,
+} = require("../utils/message_generator");
 const validateDate = require("../utils/date_validator");
 const { currentDay, currentMonth } = require("../constants");
 
@@ -21,4 +24,22 @@ const birthdayGreeting = async (req, res, next) => {
   }
 };
 
-module.exports = birthdayGreeting;
+const birthdayGreeting_gender = async (req, res, next) => {
+  try {
+    month = req.query.month || currentMonth;
+    day = req.query.day || currentDay;
+    if (!validateDate(month, day)) {
+      return res.status(400).json({ message: "Input Date is invalid" });
+    }
+    const result = await getBirthdayGreetings(month, day);
+    if (result.length === 0) {
+      return res.status(404).json({ message: "NOT FOUND" });
+    }
+    const message = await messageGeneratorV2(result);
+    res.status(200).json(message);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { birthdayGreeting, birthdayGreeting_gender };
