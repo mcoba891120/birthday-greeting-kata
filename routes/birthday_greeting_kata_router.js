b76ccef016067aa6b199cd965793dde3572e8525
mcoba891120 <119391593+mcoba891120@users.noreply.github.com>
2023-12-11 16:24:15 +0800

    feat: add route for birthday greeting with fullname

--- routes/birthday_greeting_kata_router.js
+++ routes/birthday_greeting_kata_router.js
@@ -4,10 +4,12 @@ const {
   birthdayGreeting,
   birthdayGreeting_gender,
   birthdayGreeting_age,
+  birthdayGreeting_fullname,
 } = require("../controllers/birthday_greeting_kata_controller");
 
 router.get("/v1", birthdayGreeting);
 router.get("/v2", birthdayGreeting_gender);
 router.get("/v3", birthdayGreeting_age);
+router.get("/v4", birthdayGreeting_fullname);
 
 module.exports = router;
