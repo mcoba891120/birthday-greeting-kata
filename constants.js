da5722bdfacbf678ebd0ae7a35ad140bfe1960dc
mcoba891120 <119391593+mcoba891120@users.noreply.github.com>
2023-12-11 16:09:27 +0800

    feat: add promotion messages and age validation functions

--- constants.js
+++ constants.js
@@ -1,9 +1,17 @@
 const Subject = "Happy Birthday";
 const currentDay = "08";
 const currentMonth = "08";
+const elderyAge = 49;
+const Male_promotion_message =
+  "We offer special discount 20% off for the following items: White Wine, iPhone X";
+const Fefale_promotion_message =
+  "We offer special discount 50% off for the following items: Cosmetic, LV Handbags";
 
 module.exports = {
   Subject,
   currentDay,
   currentMonth,
+  Male_promotion_message,
+  Fefale_promotion_message,
+  elderyAge,
 };
--- utils/date_validator.js
+++ utils/date_validator.js
@@ -1,6 +1,18 @@
+const { elderyAge } = require("../constants");
+
 function validateDate(month, day) {
   const date = new Date(`2023-${month}-${day}`);
   return date.getMonth() + 1 == month && date.getDate() == day;
 }
 
-module.exports = validateDate;
+function validateAge(date_of_birth) {
+  const date = new Date(date_of_birth);
+  const today = new Date();
+  const age = today.getFullYear() - date.getFullYear();
+  if (age < elderyAge) {
+    return false;
+  }
+  return true;
+}
+
+module.exports = { validateDate, validateAge };
