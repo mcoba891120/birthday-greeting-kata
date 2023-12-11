8dc77c2f76aa96fcf7380434dd379bb2ef90368d
mcoba891120 <119391593+mcoba891120@users.noreply.github.com>
2023-12-11 15:33:28 +0800

    feat: add messageGeneratorV2

--- utils/message_generator.js
+++ utils/message_generator.js
@@ -10,4 +10,22 @@ const messageGenerator = async (result) => {
   return message;
 };
 
-module.exports = messageGenerator;
+const messageGeneratorV2 = async (result) => {
+  const message = result.map((row) => {
+    let promotion_message;
+    if (row.gender === "Male") {
+      promotion_message =
+        "We offer special discount 20% off for the following items: White Wine, iPhone X";
+    } else {
+      promotion_message =
+        "We offer special discount 50% off for the following items: Cosmetic, LV Handbags";
+    }
+    return {
+      subject: Subject,
+      message: `Happy birthday, dear ${row.first_name}!`,
+      promotion_message: promotion_message,
+    };
+  });
+  return message;
+};
+module.exports = { messageGenerator, messageGeneratorV2 };
