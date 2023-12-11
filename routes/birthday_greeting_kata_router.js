892feadaeb26578b01ab54690eaeae1db52ec6bc
mcoba891120 <119391593+mcoba891120@users.noreply.github.com>
2023-12-11 16:08:45 +0800

    feat: add birthdayGreeting_age route

--- routes/birthday_greeting_kata_router.js
+++ routes/birthday_greeting_kata_router.js
@@ -3,9 +3,11 @@ var router = express.Router();
 const {
   birthdayGreeting,
   birthdayGreeting_gender,
+  birthdayGreeting_age,
 } = require("../controllers/birthday_greeting_kata_controller");
 
 router.get("/v1", birthdayGreeting);
 router.get("/v2", birthdayGreeting_gender);
+router.get("/v3", birthdayGreeting_age);
 
 module.exports = router;
