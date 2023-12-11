2525aa4bec64b2ebcded0d612562db2188931f7c
mcoba891120 <119391593+mcoba891120@users.noreply.github.com>
2023-12-12 02:34:41 +0800

    fix: fix date validation logic

--- utils/date_validator.js
+++ utils/date_validator.js
@@ -1,6 +1,6 @@
 function validateDate(month, day) {
   const date = new Date(`2023-${month}-${day}`);
-  return !isNaN(date);
+  return date.getMonth() + 1 == month && date.getDate() == day;
 }
 
 module.exports = validateDate;
