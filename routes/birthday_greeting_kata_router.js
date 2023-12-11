252a414a5855faadea8e1634abeed83debc87b02
mcoba891120 <119391593+mcoba891120@users.noreply.github.com>
2023-12-11 15:32:40 +0800

    feat: add new route for birthday greeting with gender

--- routes/birthday_greeting_kata_router.js
+++ routes/birthday_greeting_kata_router.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var router = express.Router();
-const birthdayGreeting = require("../controllers/birthday_greeting_kata_controller");
+const {
+  birthdayGreeting,
+  birthdayGreeting_gender,
+} = require("../controllers/birthday_greeting_kata_controller");
 
 router.get("/v1", birthdayGreeting);
+router.get("/v2", birthdayGreeting_gender);
 
 module.exports = router;
