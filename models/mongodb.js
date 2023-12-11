46cf530a852915b8099a983eb3b6e1bf785bc3b2
mcoba891120 <119391593+mcoba891120@users.noreply.github.com>
2023-12-11 23:09:32 +0800

    refactor: refactor MemberSchema in mongodb.js

--- models/mongodb.js
+++ models/mongodb.js
@@ -11,32 +11,36 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error("Connection error:", error));
 db.once("open", () => console.log("Database connected"));
 
-const MemberSchema = new mongoose.Schema({
-  id: {
-    type: Number,
-    required: true,
-    unique: true,
+const MemberSchema = new mongoose.Schema(
+  {
+    id: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    date_of_birth: {
+      type: Date,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
   },
-  first_name: {
-    type: String,
-    required: true,
-  },
-  last_name: {
-    type: String,
-    required: true,
-  },
-  gender: {
-    type: String,
-    required: true,
-  },
-  date_of_birth: {
-    type: Date,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-});
+  { versionKey: false },
+  { collection: "member_records" }
+);
 
 module.exports = mongoose.model("Member", MemberSchema);
